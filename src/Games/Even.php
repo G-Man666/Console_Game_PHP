bfee0483fbf5c75bb2da20e943971dfb4b6c9be3
Ch Ch <kristian-chernov2013@yandex.ru>
2024-03-05 17:18:51 +0500

    Even.php

--- src/Games/Even.php
+++ src/Games/Even.php
@@ -1,1 +1,19 @@
 <?php
+
+namespace Brain\Games\Even;
+
+use function Brain\Engine\play;
+
+const GAME_DEF = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+function run(): void
+{
+    $logic = function (): array {
+        $question = rand(1, 99);
+        $correctAnswer = ($question % 2 == 0) ? 'yes' : 'no';
+
+        return [$question, $correctAnswer];
+    };
+
+    play(GAME_DEF, $logic);
+}
