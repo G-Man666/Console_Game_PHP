a14425dc0dc4d877702ff3bc58fe6ada11aa4a3d
Ch Ch <kristian-chernov2013@yandex.ru>
2024-03-05 17:22:41 +0500

    Add Progression.php

--- src/Games/Progression.php
+++ src/Games/Progression.php
@@ -1,1 +1,30 @@
 <?php
+
+namespace Brain\Games\Progression;
+
+use function Brain\Engine\play;
+
+const GAME_DEF = 'What number is missing in the progression?';
+
+function run(): void
+{
+    $logic = function (): array {
+        $length = rand(5, 10);
+        $step = rand(2, 10);
+        $missingPosition = rand(0, $length - 1);
+        $start = rand(1, 99);
+        $result = [];
+
+        for ($i = 0; $i < $length; $i++) {
+            $result[] = $start + $i * $step;
+        }
+
+        $answer = $result[$missingPosition];
+        $result[$missingPosition] = '..';
+        $question = implode(' ', $result);
+
+        return [$question, $answer];
+    };
+
+    play(GAME_DEF, $logic);
+}
