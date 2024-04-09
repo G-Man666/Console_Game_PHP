9d96b4088fd5b877b978857193e889de6741d85c
Ch Ch <kristian-chernov2013@yandex.ru>
2024-04-09 22:48:36 +0500

    Add function calculate

--- src/Games/Calc.php
+++ src/Games/Calc.php
@@ -7,27 +7,30 @@ use function Brain\Engine\play;
 const GAME_DEF = 'What is the result of the expression?';
 const MIN_VALUE = 1;
 const MAX_VALUE = 99;
+
+function calculate(int $num1,int $num2, string $operator): int
+{
+    switch ($operator) {
+        case '+':
+            return $num1 + $num2;
+        case '-':
+            return $num1 - $num2;
+        case '*':
+            return $num1 * $num2;
+        default:
+            return 0;
+    }
+}
+
 function run(): void
 {
     $logic = function (): array {
         $num1 = rand(MIN_VALUE, MAX_VALUE);
         $num2 = rand(MIN_VALUE, MAX_VALUE);
         $operators = ['+', '-', '*'];
-        $operator = array_rand($operators);
-        $question = "{$num1} {$operators[$operator]} {$num2}";
-        $correctAnswer = 0;
-
-        switch ($operator) {
-            case 0:
-                $correctAnswer = $num1 + $num2;
-                break;
-            case 1:
-                $correctAnswer = $num1 - $num2;
-                break;
-            case 2:
-                $correctAnswer = $num1 * $num2;
-                break;
-        }
+        $operator = $operators[array_rand($operators)];
+        $question = "{$num1} {$operator} {$num2}";
+        $correctAnswer = calculate($num1, $num2, $operator);
 
         return [$question, $correctAnswer];
     };
