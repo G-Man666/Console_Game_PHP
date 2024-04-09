72f4c109a3ccadbbe503314442e9f005004430d9
Ch Ch <kristian-chernov2013@yandex.ru>
2024-04-09 23:15:02 +0500

    ADd function even

--- src/Games/Even.php
+++ src/Games/Even.php
@@ -7,11 +7,17 @@ use function Brain\Engine\play;
 const GAME_DEF = 'Answer "yes" if the number is even, otherwise answer "no".';
 const MIN_VALUE = 1;
 const MAX_VALUE = 99;
+
+function even(int $value): bool
+{
+    return $value % 2 === 0;
+}
+
 function run(): void
 {
     $logic = function (): array {
         $question = rand(MIN_VALUE, MAX_VALUE);
-        $correctAnswer = ($question % 2 === 0) ? 'yes' : 'no';
+        $correctAnswer = even($question) ? 'yes' : 'no';
 
         return [$question, $correctAnswer];
     };
